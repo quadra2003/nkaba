7c60673fa0a923da57b7520c6922ae480bb52c35
quadra2003 <56703545+quadra2003@users.noreply.github.com>
2025-02-12 04:28:15 -0800

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  images: {
+    domains: [
+      "www.spencerfane.com",
+      "ktslaw.com",
+      "www.rivkinradler.com",
+      "www.knobbe.com",
+      "www.troutman.com",
+      "bbklaw.com",
+      "www.royoulaw.com",
+      "kahanafeld.com"
+    ],
+  },
+};
