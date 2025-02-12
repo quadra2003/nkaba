676bf800e0b96315a2bb8fa12bcc209735b07e50
quadra2003 <56703545+quadra2003@users.noreply.github.com>
2025-02-12 05:41:53 -0800

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -3,9 +3,11 @@ import Layout from '../components/layout';
 
 export default function About() {
     return (
+        <Layout>
         <div style={{ padding: '2rem', textAlign: 'center' }}>
             <h2>About Us</h2>
             <p>The National Korean American Bar Association (NKABA) promotes justice, education, and collaboration among Korean American legal professionals.</p>
         </div>
+        </Layout>
     );
 }
