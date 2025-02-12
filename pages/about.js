5d7583eb51c2fd863991b4859efcafc4181ecea5
quadra2003 <56703545+quadra2003@users.noreply.github.com>
2025-02-12 06:55:25 -0800

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -4,7 +4,7 @@ import Layout from '../components/layout';
 export default function About() {
     return (
         <Layout>
-        <div style={{ padding: '2rem', textAlign: 'center' }}>
+        <div style={{ padding: '0.5rem', textAlign: 'center' }}>
             <h2>About Us</h2>
             <p>The National Korean American Bar Association (NKABA) promotes justice, education, and collaboration among Korean American legal professionals.</p>
         </div>
