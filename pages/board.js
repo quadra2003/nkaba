03710c7bbd9c4f7f48acc6d5c36dff533ccdd369
quadra2003 <56703545+quadra2003@users.noreply.github.com>
2025-02-12 04:51:13 -0800

    Update board.js

--- pages/board.js
+++ pages/board.js
@@ -1,14 +1,14 @@
 export default function Board() {
     const boardMembers = [
-        { name: "Daniel Chung", link: "https://www.spencerfane.com/professionals/daniel-chung/", image: "https://www.spencerfane.com/wp-content/uploads/2022/02/daniel-chung.jpg" },
-        { name: "Jenny Goak", link: "https://ktslaw.com/en/People/G/GoakJenny", image: "https://ktslaw.com/wp-content/uploads/2022/02/goak-jenny.jpg" },
-        { name: "Lawrence Han", link: "https://www.rivkinradler.com/attorneys/lawrence-han/", image: "https://www.rivkinradler.com/wp-content/uploads/2022/02/lawrence-han.jpg" },
-        { name: "Gene Kang", link: "https://www.rivkinradler.com/attorneys/gene-y-kang/", image: "https://www.rivkinradler.com/wp-content/uploads/2022/02/gene-kang.jpg" },
-        { name: "Chang Lim", link: "https://www.knobbe.com/professionals/chang-lim/", image: "https://www.knobbe.com/wp-content/uploads/2022/02/chang-lim.jpg" },
-        { name: "Rosa Namgoong", link: "https://www.troutman.com/professionals/rosa-namgoong.html", image: "https://www.troutman.com/wp-content/uploads/2022/02/rosa-namgoong.jpg" },
-        { name: "Jin Hee Park", link: "https://bbklaw.com/people/jin_hee_park", image: "https://bbklaw.com/wp-content/uploads/2022/02/jin-hee-park.jpg" },
-        { name: "Samuel Sung H. You", link: "https://www.royoulaw.com/samuel-h-you", image: "https://www.royoulaw.com/wp-content/uploads/2022/02/samuel-you.jpg" },
-        { name: "Samuel Yu", link: "https://kahanafeld.com/team-member/samuel-yu-esq/", image: "https://kahanafeld.com/wp-content/uploads/2022/02/samuel-yu.jpg" }
+        { name: "Daniel Chung", link: "https://www.spencerfane.com/professionals/daniel-chung/", image: "/daniel-chung.jpg" },
+        { name: "Jenny Goak", link: "https://ktslaw.com/en/People/G/GoakJenny", image: "/jenny-goak.jpg" },
+        { name: "Lawrence Han", link: "https://www.rivkinradler.com/attorneys/lawrence-han/", image: "/lawrence-han.jpg" },
+        { name: "Gene Kang", link: "https://www.rivkinradler.com/attorneys/gene-y-kang/", image: "/gene-kang.jpg" },
+        { name: "Chang Lim", link: "https://www.knobbe.com/professionals/chang-lim/", image: "/chang-lim.jpg" },
+        { name: "Rosa Namgoong", link: "https://www.troutman.com/professionals/rosa-namgoong.html", image: "/rosa-namgoong.jpg" },
+        { name: "Jin Hee Park", link: "https://bbklaw.com/people/jin_hee_park", image: "/jin-hee-park.jpg" },
+        { name: "Samuel Sung H. You", link: "https://www.royoulaw.com/samuel-h-you", image: "/samuel-you.jpg" },
+        { name: "Samuel Yu", link: "https://kahanafeld.com/team-member/samuel-yu-esq/", image: "/samuel-yu.jpg" }
     ];
 
     return (
